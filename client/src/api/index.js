5541909073de10faacda95db4ed5086228bd6557
Nikhil Jadhav <112622476+NikhilYJadhav@users.noreply.github.com>
2024-01-23 14:45:27 +0530

    Update index.js

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 // import axios from 'axios';
 
-// const url = 'http://localhost:5000/posts';
+// const url = 'https://memories-4gi6.onrender.com/posts';
 
 // export const fetchPosts = () => axios.get(url);
 // export const createPost = (newPost) => axios.post(url, newPost);
@@ -10,7 +10,7 @@
 
 import axios from 'axios';
 
-const url = 'http://localhost:5000/posts';
+const url = 'https://memories-4gi6.onrender.com/posts';
 
 export const fetchPosts = () => axios.get(url);
 export const createPost = (newPost) => axios.post(url, newPost);
